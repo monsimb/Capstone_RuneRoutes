087789333ebd38bf66da1b671fdf9990ed0f0ae6
jcl0251 <josephcleal529@gmail.com>
2025-03-24 23:40:30 -0500

    More secure db with auth. Added some params

--- src/database/db.js
+++ src/database/db.js
@@ -1,32 +1,34 @@
 // db.js (or mongoose setup file)
-
-
-// MUST BE MOVED TO ENV -> change user and password before deployment ;(
-MONGODB_URI = "mongodb+srv://db-user:QJF1Q92KFh8Uynar@cluster0.us0i8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-//
-// username password database name
-//
-
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// connect to MongoDB
-const url = MONGODB_URI; // change to env call
+const url = process.env.MONGODB_URI; // changed to env call
+
+if(!url) {
+  console.error('MONGODB URI not located. Check if you have setup .env file (root directory) with the MONGODB_URI');
+  process.exit(1);
+}
 
 // connect to MongoDB
-mongoose.connect(url)
+mongoose.connect(url, {
+  userNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.log('Error connecting to MongoDB:', err));
 
 // schema for user data
 const userSchema = new Schema({
   userId: { type: String, required: true, unique: true },  // Store user ID (from Auth0)
+  userName: { type: String, required: true, unique: true }, // Username (from Auth0)
+  avatarSelections: [String],
   coordinates: {
     lat: { type: Number, required: true },  // Latitude
     lon: { type: Number, required: true }   // Longitude
   }
-});
+}, { timestamps: true });
 
 // model for the schema
 const User = mongoose.model('User', userSchema);
